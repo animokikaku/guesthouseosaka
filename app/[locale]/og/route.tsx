46cf713961a2a45081d04c6093a1555bd5a594f1
melkir <melkir13@gmail.com>
2025-11-24 23:01:31 +0900

    fix(og-image): update logo URL and adjust image dimensions

--- app/[locale]/og/route.tsx
+++ app/[locale]/og/route.tsx
@@ -5,23 +5,18 @@ export const runtime = 'edge'
 
 export async function GET(request: Request) {
   const imageUrl = new URL('/og-image.jpg', request.url).toString()
-  const logoUrl = new URL('/android-chrome-192x192.png', request.url).toString()
+  const logoUrl = new URL('/logo.png', request.url).toString()
 
   return new ImageResponse(
     (
       <div tw="relative flex h-full w-full">
-        <img
-          src={imageUrl}
-          alt="Guest House Osaka"
-          tw="h-full w-full object-cover"
-        />
+        <img src={imageUrl} alt="Guest House Osaka" tw="h-full w-full" />
         <div tw="absolute left-4 top-4 flex items-center justify-center">
           <img
             src={logoUrl}
             alt="Guest House Osaka logo"
-            width={192}
-            height={192}
-            tw="h-48 w-48"
+            width={322}
+            height={209}
           />
         </div>
       </div>
