b48d15dc6d6fec748deaae94ca73f3e0dc9131ac
melkir <melkir13@gmail.com>
2025-12-05 18:51:35 +0900

    feat(routing): localized pathnames

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -7,10 +7,23 @@ export const routing = defineRouting({
     '/': '/',
     '/[house]': '/[house]',
     '/[house]/gallery': '/[house]/gallery',
-    '/contact': '/contact',
-    '/contact/tour': '/contact/tour',
-    '/contact/move-in': '/contact/move-in',
-    '/contact/other': '/contact/other',
-    '/faq': '/faq'
+    '/contact': {
+      ja: '/お問い合わせ'
+    },
+    '/contact/tour': {
+      ja: '/お問い合わせ/見学のご依頼',
+      fr: '/contact/visite'
+    },
+    '/contact/move-in': {
+      ja: '/お問い合わせ/入居リクエスト',
+      fr: '/contact/demenagement'
+    },
+    '/contact/other': {
+      ja: '/お問い合わせ/一般的なお問い合わせ',
+      fr: '/contact/autre'
+    },
+    '/faq': {
+      ja: '/よくある質問'
+    }
   }
 })
