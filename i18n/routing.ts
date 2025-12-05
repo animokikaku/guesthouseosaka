e37b733cb3dc6ad3e13af9de58edf56abd4ab33f
melkir <melkir13@gmail.com>
2025-12-05 18:17:38 +0900

    refacto(routing): order routes

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -5,12 +5,12 @@ export const routing = defineRouting({
   defaultLocale: 'en',
   pathnames: {
     '/': '/',
-    '/faq': '/faq',
     '/[house]': '/[house]',
     '/[house]/gallery': '/[house]/gallery',
     '/contact': '/contact',
     '/contact/tour': '/contact/tour',
     '/contact/move-in': '/contact/move-in',
-    '/contact/other': '/contact/other'
+    '/contact/other': '/contact/other',
+    '/faq': '/faq'
   }
 })
