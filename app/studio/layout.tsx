fb0a33035031e6bc627941efd2a08ca3ff5e8687
melkir <melkir13@gmail.com>
2025-12-20 03:08:30 +0900

    chore(layout): set dynamic export to 'force-static' for improved performance

--- app/studio/layout.tsx
+++ app/studio/layout.tsx
@@ -8,6 +8,8 @@ const inter = Inter({
   display: 'swap'
 })
 
+export const dynamic = 'force-static'
+
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function RootLayout({
