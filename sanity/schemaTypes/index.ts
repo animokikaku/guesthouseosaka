4d0138db190e53ef9baf47e0d6e7e7bfa8f02f75
melkir <melkir13@gmail.com>
2026-01-05 02:22:09 +0100

    refactor(sanity): reorganize schema imports and redefine homePage export
    - Updated the import structure in schemaTypes to improve clarity and maintainability by explicitly importing object types.
    - Changed the homePage export from default to named export for better consistency with other document types.
    - Removed the consolidated index file for objects, streamlining the import process and enhancing modularity.

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -8,31 +8,30 @@ import { contactType } from './documents/contact-type'
 import { faqPage } from './documents/faq-page'
 import { faqQuestion } from './documents/faq-question'
 import { galleryCategory } from './documents/gallery-category'
-import homePage from './documents/home-page'
+import { homePage } from './documents/home-page'
 import { house } from './documents/house'
 import { legalNotice } from './documents/legal-notice'
 import { pricingCategory } from './documents/pricing-category'
 import { settings } from './documents/settings'
-
 // Object types
+import { address } from './objects/address'
+import { extraCostItem } from './objects/extra-cost-item'
 import {
-  address,
-  extraCostItem,
   formFieldConfig,
-  formFieldConfigNoPlaceholder,
-  houseAmenity,
-  houseAmenityCategory,
-  houseBuilding,
-  houseGalleryCategory,
-  houseGalleryItem,
-  houseLocation,
-  houseMap,
-  housePhone,
-  localizedImage,
-  pageAction,
-  pricingRow,
-  socialLink
-} from './objects'
+  formFieldConfigNoPlaceholder
+} from './objects/form-field-config'
+import { houseAmenity } from './objects/house-amenity'
+import { houseAmenityCategory } from './objects/house-amenity-category'
+import { houseBuilding } from './objects/house-building'
+import { houseGalleryCategory } from './objects/house-gallery-category'
+import { houseGalleryItem } from './objects/house-gallery-item'
+import { houseLocation } from './objects/house-location'
+import { houseMap } from './objects/house-map'
+import { housePhone } from './objects/house-phone'
+import { localizedImage } from './objects/localized-image'
+import { pageAction } from './objects/page-action'
+import { pricingRow } from './objects/pricing-row'
+import { socialLink } from './objects/social-link'
 
 export const documentTypes = [
   homePage,
