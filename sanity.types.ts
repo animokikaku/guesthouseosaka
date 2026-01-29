8a779d7f1e3823d1373d5e6918937362ea74d6f0
melkir <melkir13@gmail.com>
2026-01-29 18:03:54 +0900

    chore: update @sanity/client to version 7.14.1 and add thumbHash attribute to schema

--- sanity.types.ts
+++ sanity.types.ts
@@ -636,6 +636,7 @@ export type SanityImageMetadata = {
   palette?: SanityImagePalette
   lqip?: string
   blurHash?: string
+  thumbHash?: string
   hasAlpha?: boolean
   isOpaque?: boolean
 }
@@ -761,6 +762,12 @@ export type AllSanitySchemaTypes =
   | SanityImageAsset
 
 export declare const internalGroqTypeReferenceTo: unique symbol
+
+type ArrayOf<T> = Array<
+  T & {
+    _key: string
+  }
+>
 
 // Source: sanity/lib/queries.ts
 // Variable: settingsQuery
