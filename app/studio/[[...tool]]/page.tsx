9228ae6fb95b150d6b7c02d0bd7d8af30e1d08d9
melkir <melkir13@gmail.com>
2026-07-26 14:16:02 +0900

    refactor: render studio directly

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,7 +1,9 @@
-import { StudioClientWrapper } from './StudioClientWrapper'
+import { NextStudio } from 'next-sanity/studio'
+
+import config from '@/sanity.config'
 
 export const dynamic = 'force-static'
 
 export default function StudioPage() {
-  return <StudioClientWrapper />
+  return <NextStudio config={config} />
 }
